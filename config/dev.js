3ac43851028f8fa3d8b7d4e673b3573bda9c26ef
Minh Nguyen <hoangminh160997@gmail.com>
2019-06-06 15:20:36 -0700

    dev and prod key

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID:
+        "408509518857-saojal6407pmiscpoarik8napgl4k29p.apps.googleusercontent.com",
+    googleClientSecret: "9ijioI21iyyyvoi0yRT4S_Td",
+    mongoDB:
+        "mongodb+srv://ADMIN-MINH:mM160997%40@cluster0-gjhw3.mongodb.net/main",
+    cookieKey: "mmmmaswdmwdon"
+};
---
+++ config/prod.js
@@ -0,0 +1,7 @@
+//prod.js do not commit
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoDB: process.env.MONGODB,
+    cookieKey: process.env.COOKIE_KEY
+};
