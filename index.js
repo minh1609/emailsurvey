29dccb825c4e167a08dea0b05f4ae33a1f3692cd
Minh Nguyen <hoangminh160997@gmail.com>
2019-06-05 22:01:40 -0700

    test server

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(5000, () => {
     console.log(`App running on ${PORT}`);
